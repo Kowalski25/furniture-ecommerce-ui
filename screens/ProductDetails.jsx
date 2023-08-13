59c483c78e51203154afd13b877d3679fe7d05c8
Kowalski25 <it.felixarhelacbang@gmail.com>
2023-08-14 04:21:01 +0800

    working on the product details ui

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,7 +1,7 @@
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image, SafeAreaView, Text } from 'react-native';
 import React from 'react';
 import styles from './productDetails.style';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, SimpleLineIcons } from '@expo/vector-icons';
 import { COLORS } from '../constants';
 
 const ProductDetails = ({ navigation }) => {
@@ -9,7 +9,7 @@ const ProductDetails = ({ navigation }) => {
     <View style={styles.container}>
       <View style={styles.upperRow}>
         <TouchableOpacity onPress={() => navigation.goBack()} >
-          <Ionicons name='chevron-back' size={30} />
+          <Ionicons name='chevron-back' size={32}  />
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => { }}>
@@ -20,6 +20,44 @@ const ProductDetails = ({ navigation }) => {
         source={{ uri: "https://d326fntlu7tb1e.cloudfront.net/uploads/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg" }}
         style={styles.image}
       />
+      <View style={styles.details}>
+
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product 1</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$249.90</Text>
+          </View>
+        </View>
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 5].map((index) => (
+              <Ionicons 
+                key={index}
+                name='star'
+                size={22}
+                color='gold'
+              />
+            ))}
+            <Text style={styles.ratingText}>(4.9)</Text>
+          </View>
+          <View style={styles.rating}>
+            <TouchableOpacity onPress={() => {}}>
+              <SimpleLineIcons 
+                name='plus' size={20}
+              />
+            </TouchableOpacity>
+
+            <Text style={styles.ratingText}> 1 </Text>
+            <TouchableOpacity onPress={() => {}}>
+              <SimpleLineIcons 
+                name='minus' size={20}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        
+
+      </View>
     </View>
   );
 };
--- screens/productDetails.style.js
+++ screens/productDetails.style.js
@@ -4,20 +4,70 @@ import { COLORS, SIZES } from "../constants";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: COLORS.lightWhite,
   },
   upperRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginHorizontal: 20,
-    position: 'absolute',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 15,
+    position: "absolute",
     top: SIZES.xxLarge,
-    width: SIZES.width - 44,
+    width: SIZES.width - 35,
     zIndex: 99,
   },
   image: {
     aspectRatio: 1,
-    resizeMode: 'cover',
+    resizeMode: "cover",
+  },
+  details: {
+    marginTop: -SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    width: SIZES.width,
+    borderTopLeftRadius: SIZES.medium,
+    borderTopRightRadius: SIZES.medium,
+  },
+  titleRow: {
+    marginHorizontal: 20,
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width - 44,
+    top: 20,
+  },
+  title: {
+    fontFamily: "bold",
+    fontSize: SIZES.large,
+  },
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large,
+  },
+  price: {
+    fontFamily: "semibold",
+    fontSize: SIZES.large,
+    paddingHorizontal: 10,
+  },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    top: 5,
+    width: SIZES.width - 10,
+  },
+  rating: {
+    top: SIZES.large,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginHorizontal: SIZES.large - 2,
+  },
+  ratingText: {
+    color: COLORS.gray,
+    fontFamily: 'medium',
+    marginHorizontal: 5,
   }
 });
 
